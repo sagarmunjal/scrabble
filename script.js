2286b769ebc3dca94e0f7a420731072b7ed1bc6e
sagarmunjal <munjal.sagar@gmail.com>
2019-04-17 18:54:03 +0530

    newGame function in creation
    check the flow chart to understand the flow of the code. 
    https://drive.google.com/file/d/1NBAx6G8X7E5yEUnr_F_B0trfl7mK9jQi/view?usp=sharing

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //taking one array first to figure out logic, will modify later
-var words1 = ["purple", "pulp", "rue", "pure"];      
+var words = [
+    ["purple", "pulp", "rue", "pure"], // game #1
+    ["banish", "bash", "nab", "bias"] // game #2
+];
+var locations = [
+    ["xxxxXxxxxxxXxxxxxxXxxxXXXXXXxXxXxxxxXxXxxxxXxxxxx",
+        "2,4,a", "5,1,d", "4,4,d", "2,4,d"], // game #1
+    ["xxxXxxxxxXXXXxXxxXxxxXxxXxxxXXXXxxxxxxXxxxxxxxxxx",
+        "4,1,d", "3,2,a", "1,3,d", "1,5,a"] // game #2
+];
+
+/*
+
 function shuffle(){
     //word has the letters of first word in chosen array.
     var word = words1[0].split('');
@@ -33,3 +45,33 @@ function createTable(){
     }
     
 }
+
+*/
+
+
+var gameId;
+function newGame(){
+
+    // write logic of creating a randomNumber on the basis of the length of the words array. 
+    gameId === 0 ? gameId = 1 : gameId = 0;
+    createTable(generateTable('crossword', 7, 7, locations[gameId][0]));
+
+    generateTable('paletteA', 1, 6);
+    // content
+        available = words[gameId][0];
+        available = available.split('');
+        // shuffle
+        available = available.sort((a,b)=> {if(a>b) {return 1 }else if(a<b) {return -1 }else {return 0} } );
+        available = available.map((data) => {
+            return {
+                value: data,
+                available: true
+            }
+        })
+        
+}
+
+newGame();
+newGame();
+newGame();
+
